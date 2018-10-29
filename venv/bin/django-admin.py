80b1ae72a10603c3524597904087064444b80fe2
yangemba <yangemba@gmail.com>
2018-10-29 02:14:49 +0200

    grade7

--- venv/bin/django-admin.py
+++
@@ -1,4 +0,0 @@
-#!/home/yan/PycharmProjects/untitled2/venv/bin/python
-# EASY-INSTALL-SCRIPT: 'Django==1.11','django-admin.py'
-__requires__ = 'Django==1.11'
-__import__('pkg_resources').run_script('Django==1.11', 'django-admin.py')
